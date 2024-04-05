59e15f88031db7ce0d61ef3054556ffa3b1ca0bf
ifbian <ifbian@gmail.com>
2024-04-05 19:34:42 +0200

    form input validation for empty values

--- src/views/RsvpView.vue
+++ src/views/RsvpView.vue
@@ -14,15 +14,61 @@
         name:'',
         email:'',
         selected:'',
-        message:''
+        message:'',
+        name_error: false,
+        email_error: false,
+        selected_error: false,
+        // message_error: false
       }
     },
     methods:{
+      validateInputs(){
+        if (this.name == '') {
+          this.name_error = true;
+        } else {
+          this.name_error = false;
+        }
+        if (this.email == '') {
+          this.email_error = true;
+        } else {
+          this.email_error = false;
+        }
+        if (this.selected == '') {
+          this.selected_error = true;
+        } else {
+          this.selected_error = false;
+        }
+        // if (this.message == '') {
+        //   this.message_error = true;
+        // } else {
+        //   this.message_error = false;
+        // }
+      },
       alertolo(){
+        this.validateInputs();
+        if (! this.isAllFieldValid){
+          return;
+        }
         alert("Name: " + this.name + "\n" + "Email: " + this.email + "\n" + "You selected: " + this.selected + "\n" + "Message: " + this.message )
       }
+    },
+    computed:{
+      isAllFieldValid(){
+        if (this.name_error) {
+          return false;
+        }
+        if (this.email_error){
+          return false;
+        }
+        if (this.selected_error){
+          return false;
+        }
+        // if (this.message_error){
+        //   return false;
+        // }
+        return true;
+      }
     }
-
   }
 </script>
 
@@ -66,19 +112,22 @@
             <div class="col">
               <label for="name" class="text-uppercase mt-3 mb-1">*Name:</label>
               <!-- <input type="text" class="form-control" id="name" name="name"> -->
-              <input v-model="name" class="form-control">
+              <input v-model="name" @input="validateInputs" class="form-control">
+              <p v-if="name_error" class="red">Name is required</p>
               <label for="email" class="text-uppercase mt-3 mb-1">*email</label>
               <!-- <input type="email" class="form-control" id="email" name="email"> -->
-              <input v-model="email" class="form-control">
+              <input v-model="email" @input="validateInputs" class="form-control">
+              <p v-if="email_error" class="red">Email is required</p>
               
               <div class="form-group option-box">
                 <label for="option" class="text-uppercase mt-3 mb-1">*I will attend to</label>
-                <select v-model="selected" multiple class="form-control" id="option" name="option">
+                <select v-model="selected" @input="validateInputs" multiple class="form-control" id="option" name="option">
                   <option value="Wedding Day">Wedding Day</option>
                   <option value="Ceremony Day">Ceremony Day</option>
                   <option value="The Party">The Party</option>
                 </select>
               </div>
+              <p v-if="selected_error" class="red">I will attend to field seleciton is required</p>
 
             </div>
             <div class="col">
